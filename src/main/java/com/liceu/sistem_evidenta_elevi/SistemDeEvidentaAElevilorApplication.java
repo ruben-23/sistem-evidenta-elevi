e00b700fe70c0b21cbae2547a8b4955fe10c7122
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-01 20:36:53 +0200

    Initial commit

---
+++ src/main/java/com/liceu/sistem_evidenta_elevi/SistemDeEvidentaAElevilorApplication.java
@@ -0,0 +1,13 @@
+package com.liceu.sistem_evidenta_elevi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemDeEvidentaAElevilorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemDeEvidentaAElevilorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liceu/sistem_evidenta_elevi/SistemDeEvidentaAElevilorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liceu.sistem_evidenta_elevi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemDeEvidentaAElevilorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
