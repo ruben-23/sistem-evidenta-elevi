db796dfbd14e76091f09d757011f0c99ae878ee9
TerpeVictor-48 <188453434+TerpeVictor-48@users.noreply.github.com>
2024-12-02 17:18:59 +0200

    Adaugare get-uri si set-uri
    la entitate note

--- src/main/java/com/liceu/sistem_evidenta_elevi/entity/Note.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/entity/Note.java
@@ -21,3 +21,44 @@ public class Note {
 
     @Column(nullable = false)
     private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdElev() {
+        return idElev;
+    }
+
+    public void setidElev(Long idElev) {
+        this.idElev = idElev;
+    }
+
+    public Long getidmaterie() {
+        return idmaterie;
+    }
+
+    public void setidmaterie(Long idmaterie) {
+        this.idmaterie = idmaterie;
+    }
+
+    public Integer getnota() {
+        return nota;
+    }
+
+    public void setnota(Integer nota) {
+        this.nota = nota;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
