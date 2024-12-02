fbc900911f34be9deb323cfec296f6fa303735cc
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-02 23:03:25 +0200

    Adaugare repository pentru user, rol si permisiune

---
+++ src/main/java/com/liceu/sistem_evidenta_elevi/repository/PermisiuneRepository.java
@@ -0,0 +1,7 @@
+package com.liceu.sistem_evidenta_elevi.repository;
+
+import com.liceu.sistem_evidenta_elevi.entity.Permisiune;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermisiuneRepository extends JpaRepository<Permisiune, Integer> {
+}
