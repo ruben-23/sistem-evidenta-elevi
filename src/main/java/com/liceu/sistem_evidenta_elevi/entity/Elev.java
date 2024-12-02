4c12b318fdef1031f2076f6f8d5d250d5228a701
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-02 09:45:36 +0200

    Adaugare getteri si setteri

--- src/main/java/com/liceu/sistem_evidenta_elevi/entity/Elev.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/entity/Elev.java
@@ -34,4 +34,68 @@ public class Elev {
     @Column(nullable = false)
     private Date dataNasteriiElev;
 
+
+    public Integer getIdElev() {
+        return idElev;
+    }
+
+    public void setIdElev(Integer idElev) {
+        this.idElev = idElev;
+    }
+
+    public String getNumeElev() {
+        return numeElev;
+    }
+
+    public void setNumeElev(String numeElev) {
+        this.numeElev = numeElev;
+    }
+
+    public String getPrenumeElev() {
+        return prenumeElev;
+    }
+
+    public void setPrenumeElev(String prenumeElev) {
+        this.prenumeElev = prenumeElev;
+    }
+
+    public String getCNP() {
+        return CNP;
+    }
+
+    public void setCNP(String CNP) {
+        this.CNP = CNP;
+    }
+
+    public String getSexElev() {
+        return sexElev;
+    }
+
+    public void setSexElev(String sexElev) {
+        this.sexElev = sexElev;
+    }
+
+    public String getNumarTelefonElev() {
+        return numarTelefonElev;
+    }
+
+    public void setNumarTelefonElev(String numarTelefonElev) {
+        this.numarTelefonElev = numarTelefonElev;
+    }
+
+    public String getAdresaElev() {
+        return adresaElev;
+    }
+
+    public void setAdresaElev(String adresaElev) {
+        this.adresaElev = adresaElev;
+    }
+
+    public Date getDataNasteriiElev() {
+        return dataNasteriiElev;
+    }
+
+    public void setDataNasteriiElev(Date dataNasteriiElev) {
+        this.dataNasteriiElev = dataNasteriiElev;
+    }
 }
