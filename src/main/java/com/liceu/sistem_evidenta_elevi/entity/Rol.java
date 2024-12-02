f0adfe13df305103724507fa9897fc3a6ce1b38e
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-02 22:51:48 +0200

    Adaugare entitati pentru user, rol si permisiune

---
+++ src/main/java/com/liceu/sistem_evidenta_elevi/entity/Rol.java
@@ -0,0 +1,64 @@
+package com.liceu.sistem_evidenta_elevi.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="rol")
+public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRol;
+
+    @Column(nullable=false)
+    private String nume;
+
+    // tabel pentru a reprezenta relatia de many-to-many dintre roluri si permisiuni
+    @ManyToMany
+    @JoinTable(
+            name = "roluri_permisiuni",
+            joinColumns = @JoinColumn(name="id_rol"),
+            inverseJoinColumns = @JoinColumn(name="id_permisiune")
+    )
+    private Set<Permisiune> permisiuni;
+
+
+    @OneToMany(mappedBy = "rol")
+    private Set<User> useri = new HashSet<>();
+
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public Set<Permisiune> getPermisiuni() {
+        return permisiuni;
+    }
+
+    public void setPermisiuni(Set<Permisiune> permisiuni) {
+        this.permisiuni = permisiuni;
+    }
+
+    public Set<User> getUseri() {
+        return useri;
+    }
+
+    public void setUseri(Set<User> useri) {
+        this.useri = useri;
+    }
+}
---
+++ src/main/java/com/liceu/sistem_evidenta_elevi/entity/User.java
@@ -0,0 +1,65 @@
+package com.liceu.sistem_evidenta_elevi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int idUser;
+
+    @Column(nullable=false)
+    private String username;
+
+    @Column(nullable=false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name="id_rol", nullable=false)
+    private Rol rol;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+}
