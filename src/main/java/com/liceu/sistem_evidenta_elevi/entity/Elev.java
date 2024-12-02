b45ad144b8303b1cb188626e007bbce200be8b39
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-02 09:29:25 +0200

    Adaugare entitate elev

---
+++ src/main/java/com/liceu/sistem_evidenta_elevi/entity/Elev.java
@@ -0,0 +1,37 @@
+package com.liceu.sistem_evidenta_elevi.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+
+@Table(name="elevi")
+public class Elev {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long idElev;
+
+    @Column(nullable = false)
+    private String numeElev;
+
+    @Column(nullable = false)
+    private String prenumeElev;
+
+    @Column(nullable = false)
+    private String CNP;
+
+    @Column(nullable = false)
+    private String sexElev;
+
+    @Column(nullable = false)
+    private String numarTelefonElev;
+
+    @Column(nullable = false)
+    private String adresaElev;
+
+    @Column(nullable = false)
+    private Date dataNasteriiElev;
+
+}
