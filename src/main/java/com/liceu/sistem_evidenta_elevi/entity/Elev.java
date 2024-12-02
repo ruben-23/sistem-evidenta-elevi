a22983fc25a1a8600568b2b1ba952ea6d8930923
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-02 09:32:31 +0200

    Schimbare tip variabila

--- src/main/java/com/liceu/sistem_evidenta_elevi/entity/Elev.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/entity/Elev.java
@@ -11,7 +11,7 @@ public class Elev {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long idElev;
+    private Integer idElev;
 
     @Column(nullable = false)
     private String numeElev;
