8544fcc7a2d1e75a199d70de2ccc679f5e4c032d
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-03 15:17:09 +0200

    Adaugare metoda de query in rol repository

--- src/main/java/com/liceu/sistem_evidenta_elevi/repository/RolRepository.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/repository/RolRepository.java
@@ -4,4 +4,7 @@ import com.liceu.sistem_evidenta_elevi.entity.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    Rol findByNume(String nume);
+
 }
