158e547a946aac41505ffe459435983eb4fb3620
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-03 19:03:49 +0200

    Schimbare tip parametrii pt a accepta DTO

--- src/main/java/com/liceu/sistem_evidenta_elevi/service/ElevService.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/service/ElevService.java
@@ -1,18 +1,13 @@
 package com.liceu.sistem_evidenta_elevi.service;
 
+import com.liceu.sistem_evidenta_elevi.dto.ElevRequestDTO;
 import com.liceu.sistem_evidenta_elevi.entity.Elev;
 
 import java.util.List;
 
 public interface ElevService {
-
     List<Elev> getAllElevi();
-
     Elev getElevById(Integer id);
-
     Elev actualizareElev(Elev elev);
-
-    Elev adaugaElev(Elev elev);
-
-
+    Elev adaugaElev(ElevRequestDTO elevRequest);
 }
--- src/main/java/com/liceu/sistem_evidenta_elevi/service/UserService.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/service/UserService.java
@@ -1,5 +1,6 @@
 package com.liceu.sistem_evidenta_elevi.service;
 
+import com.liceu.sistem_evidenta_elevi.dto.UserRequestDTO;
 import com.liceu.sistem_evidenta_elevi.entity.User;
 
 import java.util.List;
@@ -7,6 +8,6 @@ import java.util.List;
 public interface UserService {
     List<User> getAllUseri();
     User getUserById(Integer id);
-    User adaugaUser(User user);
+    User adaugaUser(UserRequestDTO userRequest);
     User actualizeazaUser(User user);
 }
