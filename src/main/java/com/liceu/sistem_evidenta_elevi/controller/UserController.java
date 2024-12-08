fb5dc2edcc61383fbd91fb151edacee7aac2fe70
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-08 23:52:46 +0200

    Modificare UserController pentru a folosi DI prin constructor

--- src/main/java/com/liceu/sistem_evidenta_elevi/controller/UserController.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/controller/UserController.java
@@ -18,11 +18,9 @@ public class UserController {
     private UserService userService;
 
     @Autowired
-    public void setUserService(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    public UserController() {}
 
     @GetMapping
     public ResponseEntity<List<User>> getUseri() {
