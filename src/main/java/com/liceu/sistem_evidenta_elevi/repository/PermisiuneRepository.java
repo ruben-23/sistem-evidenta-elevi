6653ec0354cfc4d3c4a3a2084dbb636471161bdc
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-08 22:05:25 +0200

    Stergere roluri si permisiuni

--- src/main/java/com/liceu/sistem_evidenta_elevi/repository/PermisiuneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.liceu.sistem_evidenta_elevi.repository;
-
-import com.liceu.sistem_evidenta_elevi.entity.Permisiune;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermisiuneRepository extends JpaRepository<Permisiune, Integer> {
-}
