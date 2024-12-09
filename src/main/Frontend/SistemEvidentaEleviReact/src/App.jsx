c2781ef11b76f795ce34a6f0bc00cd0c394301ca
TerpeVictor-48 <188453434+TerpeVictor-48@users.noreply.github.com>
2024-12-09 10:56:51 +0200

    Modificare App jsx si css

--- src/main/Frontend/SistemEvidentaEleviReact/src/App.jsx
+++ src/main/Frontend/SistemEvidentaEleviReact/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import './App.css';
+import Login from './Views/Login.jsx';
+import AdminDashboard from './Views/AdminDashboard';
+import ProfesorDashboard from './Views/ProfesorDashboard';
+import ElevDashboard from './Views/ElevDashboard';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    // eslint-disable-next-line no-unused-vars
+    const [loggedInUser, setLoggedInUser] = useState(null);
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const handleLoginSuccess = (user) => {
+        setLoggedInUser(user);
+        if (user.role === 'ADMIN') {
+            navigate('/admin');
+        } else if (user.role === 'PROFESOR') {
+            navigate('/profesor');
+        } else if (user.role === 'ELEV') {
+            navigate('/elev');
+        }
+    };
 
-export default App
+    return (
+        <div className="app">
+            {/*{!loggedInUser ? (*/}
+            {/*    <h3 className="welcome-message">Welcome, User!</h3>*/}
+            {/*) : (*/}
+            {/*    <h3 className="welcome-message">Welcome, {loggedInUser.role}!</h3>*/}
+            {/*)}*/}
+
+            <Routes>
+                <Route path="/" element={<Login onLoginSuccess={handleLoginSuccess} />} />
+                <Route
+                    path="/admin"
+                    element={<AdminDashboard />}
+                />
+                <Route
+                    path="/profesor"
+                    element={<ProfesorDashboard />}
+                />
+                <Route path="/elev" element={<ElevDashboard />} />
+            </Routes>
+        </div>
+    );
+};
+
+export default App;
