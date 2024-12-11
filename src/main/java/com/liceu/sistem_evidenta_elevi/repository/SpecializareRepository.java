3eafe63b67962bad2e01d3b7d7806e43a8064bf4
Razvan29387 <razsalnicean08@gmail.com>
2024-12-11 16:56:01 +0200

    respository pentru materie,specializare si clasa

---
+++ src/main/java/com/liceu/sistem_evidenta_elevi/repository/SpecializareRepository.java
@@ -0,0 +1,7 @@
+package com.liceu.sistem_evidenta_elevi.repository;
+
+import com.liceu.sistem_evidenta_elevi.entity.Specializare;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecializareRepository extends JpaRepository<Specializare, Integer> {
+}
