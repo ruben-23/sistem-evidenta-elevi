e59698baf4ef7db5f50a6b79de8f644e14fc6cc7
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-11 23:34:56 +0200

    Stergere package

--- src/main/java/com/liceu/sistem_evidenta_elevi/note_elevi/NoteRepoboberitory.java
+++
@@ -1,5 +0,0 @@
-package com.liceu.sistem_evidenta_elevi.note_elevi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface NoteRepoboberitory extends JpaRepository<Note, Long> {
-}
