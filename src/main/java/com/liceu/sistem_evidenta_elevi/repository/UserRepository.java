b32276f038a3a53fc537da6b56cbaecc7a31dffc
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-13 20:29:22 +0200

    Adaugare cautare dupa username

--- src/main/java/com/liceu/sistem_evidenta_elevi/repository/UserRepository.java
+++ src/main/java/com/liceu/sistem_evidenta_elevi/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.liceu.sistem_evidenta_elevi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
