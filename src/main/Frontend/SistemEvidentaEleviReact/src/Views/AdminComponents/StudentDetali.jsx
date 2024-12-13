976d9734e75aea9287120a9a1c728c6332adcfe3
TerpeVictor-48 <188453434+TerpeVictor-48@users.noreply.github.com>
2024-12-13 13:55:57 +0200

    Variabila Constanta Schimba

--- src/main/Frontend/SistemEvidentaEleviReact/src/Views/AdminComponents/StudentDetali.jsx
+++ src/main/Frontend/SistemEvidentaEleviReact/src/Views/AdminComponents/StudentDetali.jsx
@@ -5,6 +5,12 @@ import PropTypes from 'prop-types';
 const StudentDetali = ({ student, onSave, onClose }) => {
     const [isEditable, setIsEditable] = useState(false); // To track if the fields are editable
     const [editedStudent, setEditedStudent] = useState(student); // Store the edited student data
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setEditedStudent({ ...editedStudent, [name]: value });
+    };
+
     const handleEdit = () => {
         setIsEditable(true); // Enable editing
     };
