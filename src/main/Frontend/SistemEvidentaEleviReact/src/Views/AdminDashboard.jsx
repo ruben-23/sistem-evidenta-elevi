1975336fa8d712c8492e213fb0130c61de647daf
TerpeVictor-48 <188453434+TerpeVictor-48@users.noreply.github.com>
2024-12-13 15:31:39 +0200

    Modificare editare si profesori

--- src/main/Frontend/SistemEvidentaEleviReact/src/Views/AdminDashboard.jsx
+++ src/main/Frontend/SistemEvidentaEleviReact/src/Views/AdminDashboard.jsx
@@ -116,11 +116,7 @@ const AdminDashboard = () => {
         if (type === 'students') {
             updatedData = data.students.filter((item) => item.id !== id); // Filter out the student with the given id
         } else if (type === 'teachers') {
-            setTeachers(teachers.map((teacher) =>
-                teacher.id === editingItem.item.id ? editingItem.item : teacher
-            ));
         }
-        setEditingItem(null); // Clear editing state after saving
     };
 
     // Handle Delete
