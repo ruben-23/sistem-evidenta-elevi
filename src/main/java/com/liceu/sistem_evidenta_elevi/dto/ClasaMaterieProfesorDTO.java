aa719c832065a4e0ab5c6d43458a90bb6a60a364
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-17 00:10:05 +0200

    Adaugare DTO si controller pentru clasa-materie-profesor

---
+++ src/main/java/com/liceu/sistem_evidenta_elevi/dto/ClasaMaterieProfesorDTO.java
@@ -0,0 +1,32 @@
+package com.liceu.sistem_evidenta_elevi.dto;
+
+public class ClasaMaterieProfesorDTO {
+
+    private Integer idClasa;
+    private Integer idProfesor;
+    private Integer idMaterie;
+
+    public Integer getIdClasa() {
+        return idClasa;
+    }
+
+    public void setIdClasa(Integer idClasa) {
+        this.idClasa = idClasa;
+    }
+
+    public Integer getIdProfesor() {
+        return idProfesor;
+    }
+
+    public void setIdProfesor(Integer idProfesor) {
+        this.idProfesor = idProfesor;
+    }
+
+    public Integer getIdMaterie() {
+        return idMaterie;
+    }
+
+    public void setIdMaterie(Integer idMaterie) {
+        this.idMaterie = idMaterie;
+    }
+}
