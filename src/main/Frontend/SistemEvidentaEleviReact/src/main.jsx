16ec7f22d9113218f85ccddffb44b36f74a45525
Ruben <174733720+ruben-23@users.noreply.github.com>
2025-01-02 05:10:03 +0200

    Adaugare userProvider pentru main

--- src/main/Frontend/SistemEvidentaEleviReact/src/main.jsx
+++ src/main/Frontend/SistemEvidentaEleviReact/src/main.jsx
@@ -2,9 +2,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import {UserProvider} from "./UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <UserProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </UserProvider>
 );
