543ca371a28050cb54bb264b913022b92e5747f0
Ruben <174733720+ruben-23@users.noreply.github.com>
2025-01-02 05:14:00 +0200

    Adaugare modal pentru absente

--- src/main/Frontend/SistemEvidentaEleviReact/src/Views/AdminComponents/ModalAbsente.jsx
+++ src/main/Frontend/SistemEvidentaEleviReact/src/Views/AdminComponents/ModalAbsente.jsx
@@ -1,77 +1,216 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import '../../StylesViews/StyleComponents/ModalNoteAbsente.css';
+import {fetchAbsenteElev} from "../../services/eleviService.js";
+import {actualizareAbsenta, adaugaAbsenta, stergereAbsenta} from "../../services/absentaService.js";
+import {useUser} from "../../UserContext.jsx";
 
-
-const ModalAbsente = ({ student, onClose, onSave }) => {
-    const [selectedSemester, setSelectedSemester] = useState('');
-    const [absences, setAbsences] = useState([
-        { data: '', motivare: '', gestionare: '', isNew: false, isEdited: false, isEditable: true },
+const AbsenteModal = ({ elev, onClose, onSave, materii }) => {
+    const {user} = useUser();
+    const [modulSelectat, setModulSelectat] = useState('');
+    const [materieSelectata, setMaterieSelectata] = useState('');
+    const [absente, setAbsente] = useState([
+        {
+            idAbsenta: null,
+            data: '',
+            gestionare: '',
+            isNew: true,
+            isEdited: false,
+            isEditable: true,
+            idElev: elev.idElev,
+            idMaterie: null,
+            modul: '',
+        },
     ]);
+    const [absenteFiltrate, setAbsenteFiltrate] = useState([]);
 
-    const handleSemesterChange = (e) => setSelectedSemester(e.target.value);
+    useEffect(() => {
+        const fetchAllAbsente = async () => {
+            if (elev?.idElev) {
+                try {
+                    const absenteReturnate = await fetchAbsenteElev(elev.idElev);
+                    const absenteMapate = absenteReturnate.map((absenta) => ({
+                        idAbsenta: absenta.idAbsenta,
+                        data: absenta.data,
+                        gestionare: '',
+                        isNew: false,
+                        isEdited: false,
+                        isEditable: false,
+                        modul: absenta.modul,
+                        idMaterie: absenta.idMaterie,
+                    }));
 
-    const handleAbsenceChange = (index, field, value) => {
-        const updatedAbsences = [...absences];
-        updatedAbsences[index][field] = value;
-        updatedAbsences[index].isEdited = true;
-        setAbsences(updatedAbsences);
+                    setAbsente([
+                        {
+                            idAbsenta: null,
+                            data: '',
+                            gestionare: '',
+                            isNew: true,
+                            isEdited: false,
+                            isEditable: true,
+                            idElev: elev.idElev,
+                            idMaterie: null,
+                            modul: '',
+                        },
+                        ...absenteMapate
+                    ]);
+                } catch (error) {
+                    console.error("Eroare la returnarea tuturor absentelor:", error);
+                }
+            }
+        };
+
+        fetchAllAbsente();
+    }, [elev]);
+
+
+    useEffect(() => {
+        const filtrareAbsente = () => {
+
+            const randGol = absente[0];
+
+            const filtrate = absente.slice(1).filter((absenta) => {
+                const verifModul = modulSelectat ? absenta.modul === modulSelectat : true;
+                const verifMaterie = materieSelectata ? absenta.idMaterie === materieSelectata.idMaterie : true;
+                return verifModul && verifMaterie;
+            });
+
+            setAbsenteFiltrate([randGol, ...filtrate]);
+        };
+
+        filtrareAbsente();
+    }, [absente, modulSelectat, materieSelectata]);
+
+    const handleSchimbareModul = (e) => setModulSelectat(e.target.value);
+
+    const handleSchimbareMaterie = (e) => {
+        const idSelectat = parseInt(e.target.value);
+        const materiaSelectata = materii.find((materie) => materie.idMaterie === idSelectat);
+        setMaterieSelectata(materiaSelectata || '');
     };
 
-    const handleAddAbsence = () => {
-        setAbsences([{ data: '', motivare: '', gestionare: '', isNew: true, isEdited: false, isEditable: false }, ...absences]);
+    const handleModificareAbsenta = (idAbsenta, camp, valoare) => {
+        setAbsente((prevAbsente) =>
+            prevAbsente.map((absenta) =>
+                absenta.idAbsenta === idAbsenta || (idAbsenta === null && absenta.idAbsenta === null)
+                    ? { ...absenta, [camp]: valoare, isEdited: true }
+                    : absenta
+            )
+        );
     };
 
-    const handleEditAbsence = (index) => {
-        const updatedAbsences = [...absences];
-        updatedAbsences[index].isEditable = true;
-        setAbsences(updatedAbsences);
-    };
+    const handleAdaugareAbsenta = async () => {
+        if (!modulSelectat || !materieSelectata || !absente[0].data) {
+            alert("Alegeti modulul, materia si data pentru a adauga absenta");
+            return;
+        }
 
-    const handleSaveAbsence = (index) => {
-        const updatedAbsences = [...absences];
-        updatedAbsences[index].isEditable = false;
-        updatedAbsences[index].isNew = false;
-        updatedAbsences[index].isEdited = false;
-        setAbsences(updatedAbsences);
-    };
+        const absentaNoua = {
+            idElev: elev.idElev,
+            idMaterie: materieSelectata.idMaterie,
+            data: absente[0].data,
+            modul: modulSelectat,
+        };
 
-    const handleRemoveAbsence = (index) => {
-        if (index > 0) {
-            setAbsences(absences.filter((_, i) => i !== index));
+        // resetare data
+        absente[0].data = '';
+
+        try {
+            const absentaAdaugata = await adaugaAbsenta(absentaNoua);
+
+            setAbsente((prevAbsente) => [
+                prevAbsente[0],
+                {
+                    idAbsenta: absentaAdaugata.idAbsenta,
+                    data: absentaAdaugata.data,
+                    gestionare: '',
+                    isNew: false,
+                    isEdited: false,
+                    isEditable: false,
+                    modul: absentaAdaugata.modul,
+                    idMaterie: absentaAdaugata.idMaterie,
+                },
+                ...prevAbsente.slice(1),
+            ]);
+        } catch (error) {
+            console.error("Eroare la adaugarea absentei:", error);
+            alert("A aparut o eroare la adaugarea absentei");
         }
     };
 
-    // Calculate total of new absences (excluding the default row)
-    const calculateTotal = () => {
-        return absences.filter(absence => absence.isNew).length; // Count only new rows
+    const handleSalvareAbsenta = async (idAbsenta) => {
+        const absentaGasita = absente.find((absenta) => absenta.idAbsenta === idAbsenta);
+
+        const absentaActualizata = {
+            idAbsenta: idAbsenta,
+            idElev: elev.idElev,
+            idMaterie: absentaGasita.idMaterie,
+            data: absentaGasita.data,
+            modul: absentaGasita.modul,
+        };
+
+        try {
+            await actualizareAbsenta(elev.idElev, absentaActualizata);
+            setAbsente((prevAbsente) =>
+                prevAbsente.map((absenta) =>
+                    absenta.idAbsenta === idAbsenta
+                        ? { ...absenta, isEditable: false, isNew: false, isEdited: false }
+                        : absenta
+                )
+            );
+        } catch (error) {
+            console.error("Eroare la actualizarea absentei:", error);
+            alert("A aparut o eroare la actualizarea absentei");
+        }
+    };
+
+    const handleEditareAbsenta = (idAbsenta) => {
+        setAbsente((prevAbsente) =>
+            prevAbsente.map((absenta) =>
+                absenta.idAbsenta === idAbsenta ? { ...absenta, isEditable: true } : absenta
+            )
+        );
+    };
+
+    const handleStergereAbsenta = async (idAbsenta) => {
+        try {
+            await stergereAbsenta(idAbsenta);
+            setAbsente((prevAbsente) => prevAbsente.filter((absenta) =>
+                absenta.idAbsenta !== idAbsenta));
+        } catch (error) {
+            console.error("Eroare la stergerea absentei:", error);
+            alert("A aparut o eroare la stergerea absentei");
+        }
     };
 
     return (
         <div className="modal-overlay">
             <div className="modal-content">
                 <div className="modal-header">
-                    <h3>
-                        {student.name} {student.prenume} - Absente Nemotivate: {student.absenteNemotivate}
-                    </h3>
-                    <div className="total">Total Absente: {calculateTotal()}</div>
+                    <h3>Absențe pentru {elev.nume} {elev.prenume}</h3>
+                    <button className="close-button" onClick={onClose}>&times;</button>
                 </div>
 
                 <div className="modal-body">
-                    <div className="semester-dropdown">
-                        <label htmlFor="semester">Select Semester:</label>
-                        <select
-                            id="semester"
-                            value={selectedSemester}
-                            onChange={handleSemesterChange}
-                        >
-                            <option value="">Select a semester</option>
+                    <div className="modul-dropdown">
+                        <label htmlFor="modul">Modul:</label>
+                        <select id="modul" value={modulSelectat} onChange={handleSchimbareModul}>
+                            <option value="">Selectați modulul</option>
                             <option value="Modul 1">Modul 1</option>
                             <option value="Modul 2">Modul 2</option>
                             <option value="Modul 3">Modul 3</option>
                             <option value="Modul 4">Modul 4</option>
                             <option value="Modul 5">Modul 5</option>
+                        </select>
+
+                        <label htmlFor="materii" className="materii-dropdown">Materie:</label>
+                        <select id="materii" value={materieSelectata?.idMaterie || ''} onChange={handleSchimbareMaterie}>
+                            <option value="">Selectați materia</option>
+                            {materii.map((materie) => (
+                                <option key={materie.idMaterie} value={materie.idMaterie}>
+                                    {materie.nume}
+                                </option>
+                            ))}
                         </select>
                     </div>
 
@@ -79,61 +218,60 @@ const ModalAbsente = ({ student, onClose, onSave }) => {
                         <thead>
                         <tr>
                             <th>Data</th>
-                            <th>Motivare</th>
-                            <th>Gestionare</th>
+                            <th></th>
                         </tr>
                         </thead>
                         <tbody className="scrollable-table-body">
-                        {absences.map((absence, index) => (
-                            <tr key={index}>
+                        {absenteFiltrate.map((absenta) => (
+                            <tr key={absenta.idAbsenta}>
                                 <td>
                                     <input
                                         type="date"
-                                        value={absence.data}
+                                        style={{ fontWeight: "600" }}
+                                        className="form-control bg-gradient-secondary"
+                                        value={absenta.data}
                                         onChange={(e) =>
-                                            handleAbsenceChange(index, 'data', e.target.value)
+                                            handleModificareAbsenta(absenta.idAbsenta, 'data', e.target.value)
                                         }
-                                        disabled={!absence.isEditable && index !== 0}
+                                        disabled={!absenta.isEditable}
                                     />
                                 </td>
                                 <td>
-                                    <input
-                                        type="text"
-                                        value={absence.motivare}
-                                        onChange={(e) =>
-                                            handleAbsenceChange(index, 'motivare', e.target.value)
-                                        }
-                                        disabled={!absence.isEditable && index !== 0}
-                                    />
-                                </td>
-                                <td>
-                                    {index === 0 ? (
-                                        <button onClick={handleAddAbsence}>Adauga Absenta</button>
+                                    {absenta.idAbsenta === null ? (
+                                        <button className="btn btn-warning" onClick={handleAdaugareAbsenta}>Adaugă Absența</button>
                                     ) : (
-                                        <>
-                                            {absence.isEditable ? (
-                                                <button onClick={() => handleSaveAbsence(index)}>
-                                                    Salveaza
+                                        user.rol === "ROLE_SECRETARA" && (
+                                            <>
+                                                {absenta.isEditable ? (
+                                                    <button className="btn btn-success" onClick={() => handleSalvareAbsenta(absenta.idAbsenta)}>
+                                                        Salvează
+                                                    </button>
+                                                ) : (
+                                                    <button className="btn btn-secondary" onClick={() => handleEditareAbsenta(absenta.idAbsenta)}>
+                                                        Editează
+                                                    </button>
+                                                )}
+
+                                                <button
+                                                    onClick={() => handleStergereAbsenta(absenta.idAbsenta)}
+                                                    className="btn btn-danger"
+                                                >
+                                                    Șterge
                                                 </button>
-                                            ) : (
-                                                <button onClick={() => handleEditAbsence(index)}>
-                                                    Editeaza
-                                                </button>
-                                            )}
-                                        </>
+                                            </>
+                                        )
                                     )}
-                                    <button onClick={() => handleRemoveAbsence(index)} disabled={index === 0}>
-                                        Sterge
-                                    </button>
                                 </td>
                             </tr>
                         ))}
                         </tbody>
                     </table>
 
+                    <h4>Absente: {absenteFiltrate.length-1}</h4>
+
                     <div className="modal-footer">
-                        <button onClick={onSave}>Salveaza</button>
-                        <button onClick={onClose}>Inchide</button>
+                        {/*<button onClick={onSave}>Salveaza</button>*/}
+                        {/*<button onClick={onClose}>Inchide</button>*/}
                     </div>
                 </div>
             </div>
@@ -141,15 +279,18 @@ const ModalAbsente = ({ student, onClose, onSave }) => {
     );
 };
 
-// PropTypes validation
-ModalAbsente.propTypes = {
-    student: PropTypes.shape({
-        name: PropTypes.string.isRequired,
+AbsenteModal.propTypes = {
+    elev: PropTypes.shape({
+        idElev: PropTypes.number.isRequired,
+        nume: PropTypes.string.isRequired,
         prenume: PropTypes.string.isRequired,
-        absenteNemotivate: PropTypes.string.isRequired,
     }).isRequired,
     onClose: PropTypes.func.isRequired,
     onSave: PropTypes.func.isRequired,
+    materii: PropTypes.arrayOf(PropTypes.shape({
+        idMaterie: PropTypes.number.isRequired,
+        nume: PropTypes.string.isRequired,
+    })).isRequired,
 };
 
-export default ModalAbsente;
+export default AbsenteModal;
