98f0527147ba8b935a7cab2e957ead08bf9f58fe
Ruben <174733720+ruben-23@users.noreply.github.com>
2025-01-02 05:11:50 +0200

    Modificare login pentru a incarca userul primit in localStorage + imbunatatire css

--- src/main/Frontend/SistemEvidentaEleviReact/src/Views/Login.jsx
+++ src/main/Frontend/SistemEvidentaEleviReact/src/Views/Login.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import PropTypes from 'prop-types'; 
 import '../StylesViews/Login.css';
+import {useUser} from "../UserContext.jsx";
 
-const Login = ({ onLoginSuccess }) => {
+const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const [passwordVisible, setPasswordVisible] = useState(false);  
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const navigate = useNavigate();
+
+  const { setUser  } = useUser ();
 
   const loginUser  = async (username, password) => {
     try {
@@ -39,9 +41,9 @@ const Login = ({ onLoginSuccess }) => {
     try {
       const user = await loginUser(username, password);
 
-      onLoginSuccess(user);
+      setUser(user);
 
-      if (user.rol === 'ROLE_ADMIN') {
+      if (user.rol === 'ROLE_SECRETARA') {
         navigate('/admin');
       } else if (user.rol === 'ROLE_PROFESOR') {
         navigate('/profesor');
@@ -54,49 +56,45 @@ const Login = ({ onLoginSuccess }) => {
   };
 
   return (
-    <div className="login-container">
-      <div className="form-wrapper">
-        <h2 className="header">Autentificare</h2>
-        {error && <p className="error">{error}</p>}
-        <form onSubmit={handleLogin}>
-          <div className="input-group">
-            <label className="label">Nume Utilizator:</label>
-            <input
-              className="input"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label className="label">Parola:</label>
-            <div className="password-wrapper">
+      <div className="login-container">
+        <div className="form-wrapper">
+          <h2 className="header">Autentificare</h2>
+          {error && <p className="error">{"Nume utilizator sau parola incorecte"}</p>}
+          <form onSubmit={handleLogin}>
+            <div className="input-group">
+              <label className="label">Nume Utilizator:</label>
               <input
-                className="input"
-                type={passwordVisible ? 'text' : 'password'}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
+                  className="input"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
               />
-              <button
-                type="button"
-                className="eye-button"
-                onClick={() => setPasswordVisible(!passwordVisible)}
-              >
-                {passwordVisible ? '🙈' : '👁️'}
-              </button>
             </div>
-          </div>
-          <button className="button" type="submit">Autentificare</button>
-        </form>
+            <div className="input-group">
+              <label className="label">Parola:</label>
+              <div className="password-wrapper">
+                <input
+                    className="input"
+                    type={passwordVisible ? 'text' : 'password'}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button
+                    type="button"
+                    className="eye-button"
+                    onClick={() => setPasswordVisible(!passwordVisible)}
+                >
+                  {passwordVisible ? '🙈' : '👁️'}
+                </button>
+              </div>
+            </div>
+            <button className="btn btn-warning button" >Autentificare</button>
+          </form>
+        </div>
       </div>
-    </div>
   );
-};
-
-Login.propTypes = {
-  onLoginSuccess: PropTypes.func.isRequired,
 };
 
 export default Login;
