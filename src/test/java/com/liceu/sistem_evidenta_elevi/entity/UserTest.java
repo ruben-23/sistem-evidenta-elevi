e799480d6a802ef3c7497971dd9f4361a92de0f2
Razvan29387 <razsalnicean08@gmail.com>
2025-01-03 18:28:25 +0200

    Test pentru entitati

--- src/test/java/com/liceu/sistem_evidenta_elevi/entity/UserTest.java
+++ src/test/java/com/liceu/sistem_evidenta_elevi/entity/UserTest.java
@@ -7,19 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserTest {
 
     @Test
-    public void creareUserTest(){
+    public void creareUserTest() {
         User user = new User();
-        user.setIdUser(1);
-        user.setUsername("username");
-        user.setParola("Parola");
-        user.setEmail("email");
+        user.setIdUser (1);
+        user.setUsername("ion.popescu");
+        user.setParola("parola123");
+        user.setEmail("ion.popescu@exemplu.com");
+        user.setRol(Rol.ROLE_PROFESOR);
+
+
+        Profesor profesor = new Profesor();
+        profesor.setIdProfesor(1);
+        user.setProfesor(profesor);
+
+
+        Secretara secretara = new Secretara();
+        secretara.setIdSecretara(1);
+        user.setSecretara(secretara);
+
 
         assertNotNull(user);
-        assertEquals(1, user.getIdUser());
-        assertEquals("username", user.getUsername());
-        assertEquals("Parola", user.getParola());
-        assertEquals("email", user.getEmail());
-
+        assertEquals(1, user.getIdUser ());
+        assertEquals("ion.popescu", user.getUsername());
+        assertEquals("parola123", user.getParola());
+        assertEquals("ion.popescu@exemplu.com", user.getEmail());
+        assertEquals(Rol.ROLE_PROFESOR, user.getRol());
+        assertNotNull(user.getProfesor());
+        assertEquals(1, user.getProfesor().getIdProfesor());
+        assertNotNull(user.getSecretara());
+        assertEquals(1, user.getSecretara().getIdSecretara());
     }
-
 }
