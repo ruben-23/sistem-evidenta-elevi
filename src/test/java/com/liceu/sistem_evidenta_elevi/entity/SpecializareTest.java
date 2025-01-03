bddae71fc8044837e4f6fd7193086002a543a91f
Razvan29387 <razsalnicean08@gmail.com>
2025-01-03 18:27:46 +0200

    Test pentru entitati

---
+++ src/test/java/com/liceu/sistem_evidenta_elevi/entity/SpecializareTest.java
@@ -0,0 +1,33 @@
+package com.liceu.sistem_evidenta_elevi.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpecializareTest {
+
+    @Test
+    public void creareSpecializareTest() {
+        Specializare specializare = new Specializare();
+        specializare.setIdSpecializare(1);
+        specializare.setNume("Matematica");
+
+
+        Set<Clasa> clase = new HashSet<>();
+        Clasa clasa1 = new Clasa();
+        clasa1.setIdClasa(1);
+        clase.add(clasa1);
+        specializare.setClase(clase);
+
+
+        assertNotNull(specializare);
+        assertEquals(1, specializare.getIdSpecializare());
+        assertEquals("Matematica", specializare.getNume());
+        assertNotNull(specializare.getClase());
+        assertEquals(1, specializare.getClase().size());
+        assertEquals(1, specializare.getClase().iterator().next().getIdClasa());
+    }
+}
