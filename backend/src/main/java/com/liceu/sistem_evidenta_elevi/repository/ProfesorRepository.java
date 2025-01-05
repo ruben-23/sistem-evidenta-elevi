0d6aaef8e0948f3a134c77851f5d68b36732f028
BusecanAntonio <xmaxxantono@yahoo.com>
2025-01-05 17:21:43 +0200

    adaugare javadoc ProfesorRepository

--- backend/src/main/java/com/liceu/sistem_evidenta_elevi/repository/ProfesorRepository.java
+++ backend/src/main/java/com/liceu/sistem_evidenta_elevi/repository/ProfesorRepository.java
@@ -3,5 +3,9 @@ package com.liceu.sistem_evidenta_elevi.repository;
 import com.liceu.sistem_evidenta_elevi.entity.Profesor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository pentru entitatea Profesor.
+ * Aceasta interfata extinde JpaRepository si furnizeaza operatiuni standard de CRUD pentru entitatea Profesor.
+ */
 public interface ProfesorRepository extends JpaRepository<Profesor, Integer> {
 }
