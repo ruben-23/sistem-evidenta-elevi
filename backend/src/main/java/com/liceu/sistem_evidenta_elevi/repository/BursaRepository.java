9ad21dcd3f476988addac8a352ce9941fd3784fc
BusecanAntonio <xmaxxantono@yahoo.com>
2025-01-05 17:17:04 +0200

    adaugare javadoc BursaRepository

--- backend/src/main/java/com/liceu/sistem_evidenta_elevi/repository/BursaRepository.java
+++ backend/src/main/java/com/liceu/sistem_evidenta_elevi/repository/BursaRepository.java
@@ -3,5 +3,11 @@ package com.liceu.sistem_evidenta_elevi.repository;
 import com.liceu.sistem_evidenta_elevi.entity.Bursa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository pentru entitatea Bursa.
+ *
+ * Aceasta interfata extinde JpaRepository si furnizeaza operatiuni standard pentru gestionarea entitatii Bursa
+ * in baza de date, cum ar fi salvarea, stergerea si cautarea burselor.
+ */
 public interface BursaRepository extends JpaRepository<Bursa, Integer> {
 }
