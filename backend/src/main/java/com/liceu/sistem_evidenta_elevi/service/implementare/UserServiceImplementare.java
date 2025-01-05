d102c1aa7c4ae67e599ac96abc941785e242abea
BusecanAntonio <xmaxxantono@yahoo.com>
2025-01-05 17:24:56 +0200

    adaugare javadoc UserServiceImplementare

--- backend/src/main/java/com/liceu/sistem_evidenta_elevi/service/implementare/UserServiceImplementare.java
+++ backend/src/main/java/com/liceu/sistem_evidenta_elevi/service/implementare/UserServiceImplementare.java
@@ -18,6 +18,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
+/**
+ * Implementare a serviciului User.
+ * Contine metode pentru gestionarea operatiunilor legate de utilizatori.
+ */
 @Service
 public class UserServiceImplementare implements UserService {
 
@@ -26,6 +30,14 @@ public class UserServiceImplementare implements UserService {
     private final SecretaraService secretaraService;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Constructor pentru injectarea dependintelor.
+     *
+     * @param userRepository Repositorul pentru gestionarea utilizatorilor.
+     * @param profesorService Serviciul pentru gestionarea profesorilor.
+     * @param passwordEncoder Encoderul pentru criptarea parolelor utilizatorilor.
+     * @param secretaraService Serviciul pentru gestionarea secretarelor.
+     */
     @Autowired
     public UserServiceImplementare(UserRepository userRepository, ProfesorService profesorService,
                                    PasswordEncoder passwordEncoder, SecretaraService secretaraService) {
@@ -35,22 +47,46 @@ public class UserServiceImplementare implements UserService {
         this.secretaraService = secretaraService;
     }
 
+    /**
+     * Obtine toti utilizatorii din sistem.
+     *
+     * @return Lista tuturor utilizatorilor.
+     */
     @Override
     public List<User> getAllUseri() {
         return userRepository.findAll();
     }
 
+    /**
+     * Obtine un utilizator dupa ID.
+     *
+     * @param id ID-ul utilizatorului.
+     * @return Utilizatorul corespunzator ID-ului.
+     * @throws RuntimeException Daca utilizatorul nu este gasit.
+     */
     @Override
     public User getUserById(Integer id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Userul nu a fost gasit"));
     }
 
+    /**
+     * Obtine un utilizator dupa username.
+     *
+     * @param username Numele de utilizator.
+     * @return Utilizatorul corespunzator username-ului.
+     */
     @Override
     public User getUserByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
+    /**
+     * Actualizeaza informatiile unui utilizator.
+     *
+     * @param user DTO-ul care contine datele actualizate ale utilizatorului.
+     * @return Utilizatorul actualizat.
+     */
     @Transactional
     @Override
     public User actualizeazaUser(UserRequestDTO user) {
@@ -60,8 +96,8 @@ public class UserServiceImplementare implements UserService {
         userActual.setEmail(user.getEmail());
         userActual.setRol(Rol.valueOf(user.getRol()));
 
-        if(user.getProfesor() != null) {
-            // setare id user in profesorDTO
+        // Actualizeaza profesor sau secretara daca este cazul
+        if (user.getProfesor() != null) {
             user.getProfesor().setIdUser(user.getIdUser());
             profesorService.actualizareProfesor(user.getProfesor());
         } else if (user.getSecretara() != null) {
@@ -72,6 +108,13 @@ public class UserServiceImplementare implements UserService {
         return userRepository.save(userActual);
     }
 
+    /**
+     * Adauga un nou utilizator in sistem.
+     *
+     * @param userRequest DTO-ul care contine datele utilizatorului de adaugat.
+     * @return Utilizatorul adaugat.
+     * @throws IllegalArgumentException Daca rolul nu este valid.
+     */
     @Transactional
     @Override
     public User adaugaUser(UserRequestDTO userRequest) {
@@ -79,39 +122,32 @@ public class UserServiceImplementare implements UserService {
             throw new IllegalArgumentException("User nu poate fi null");
         }
 
-        // creare user nou din DTO
+        // Creaza un utilizator nou
         User user = new User();
         user.setUsername(userRequest.getUsername());
         user.setParola(passwordEncoder.encode(userRequest.getParola()));
         user.setEmail(userRequest.getEmail());
 
-        // verifica daca rolul exista in enum
+        // Verifica daca rolul exista in enum
         String numeRol = userRequest.getRol().toUpperCase();
-        if(!Rol.exista(numeRol)) {
+        if (!Rol.exista(numeRol)) {
             throw new IllegalArgumentException("Rolul nu exista");
         }
 
-        // rolul obtinut e atribuit user-ului
+        // Atribuie rolul utilizatorului
         Rol rol = Rol.valueOf(numeRol);
         user.setRol(rol);
 
-        // creare entitate in functie de rolul din user
-        switch(rol) {
-
+        // Creaza entitate in functie de rolul utilizatorului
+        switch (rol) {
             case ROLE_PROFESOR:
-                // obtinem profesorDTO din userDTO
                 ProfesorDTO profesorRequest = userRequest.getProfesor();
-
-                // obtinem profesorul dupa adaugare
                 Profesor profesor = profesorService.adaugaProfesor(profesorRequest, user);
-
-                // legatura intre user si profesor
                 user.setProfesor(profesor);
                 break;
 
             case ROLE_SECRETARA:
                 SecretaraDTO secretaraDTO = userRequest.getSecretara();
-
                 Secretara secretara = secretaraService.adaugaSecretara(secretaraDTO, user);
                 user.setSecretara(secretara);
                 break;
@@ -123,9 +159,13 @@ public class UserServiceImplementare implements UserService {
         return userRepository.save(user);
     }
 
+    /**
+     * Sterge un utilizator din sistem pe baza ID-ului.
+     *
+     * @param idUser ID-ul utilizatorului de sters.
+     */
     @Override
-    public void stergeUser(Integer idUser){
+    public void stergeUser(Integer idUser) {
         userRepository.deleteById(idUser);
     }
-
 }
