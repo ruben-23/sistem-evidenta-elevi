62ed1443a18b65368f6f0cba4c6e00e4be46e627
BusecanAntonio <xmaxxantono@yahoo.com>
2025-01-05 17:21:00 +0200

    adaugare javadoc NotaMapper

--- backend/src/main/java/com/liceu/sistem_evidenta_elevi/mapper/NotaMapper.java
+++ backend/src/main/java/com/liceu/sistem_evidenta_elevi/mapper/NotaMapper.java
@@ -1,6 +1,5 @@
 package com.liceu.sistem_evidenta_elevi.mapper;
 
-
 import com.liceu.sistem_evidenta_elevi.dto.NotaDTO;
 import com.liceu.sistem_evidenta_elevi.entity.Elev;
 import com.liceu.sistem_evidenta_elevi.entity.Materie;
@@ -23,12 +22,24 @@ public class NotaMapper {
     private ElevService elevService;
     private MaterieService materieService;
 
+    /**
+     * Constructorul clasei NotaMapper.
+     *
+     * @param elevService Serviciul care permite obtinerea unui elev pe baza ID-ului.
+     * @param materieService Serviciul care permite obtinerea unei materii pe baza ID-ului.
+     */
     @Autowired
     public NotaMapper(ElevService elevService, MaterieService materieService) {
         this.elevService = elevService;
         this.materieService = materieService;
     }
 
+    /**
+     * Converteste un obiect de tip Nota intr-un obiect DTO de tip NotaDTO.
+     *
+     * @param nota Entitatea Nota care trebuie convertită într-un DTO.
+     * @return Un obiect NotaDTO populat cu datele din entitatea Nota.
+     */
     public NotaDTO toDTO(Nota nota) {
         NotaDTO notaDTO = new NotaDTO();
 
@@ -42,6 +53,12 @@ public class NotaMapper {
         return notaDTO;
     }
 
+    /**
+     * Convertește o lista de obiecte Nota intr-o lista de obiecte NotaDTO.
+     *
+     * @param note Lista de entitati Nota care trebuie convertita intr-o listă de DTO-uri.
+     * @return O lista de obiecte NotaDTO populata cu datele din lista de entitati Nota.
+     */
     public List<NotaDTO> toDTOList(List<Nota> note) {
         List<NotaDTO> notaDTOs = new ArrayList<>();
         for (Nota nota : note) {
@@ -50,6 +67,12 @@ public class NotaMapper {
         return notaDTOs;
     }
 
+    /**
+     * Converteste un obiect DTO de tip NotaDTO intr-o entitate de tip Nota.
+     *
+     * @param notaDTO DTO-ul Nota care trebuie convertit intr-o entitate Nota.
+     * @return O entitate Nota populata cu datele din DTO-ul NotaDTO.
+     */
     public Nota toEntity(NotaDTO notaDTO) {
 
         Elev elev = elevService.getElevById(notaDTO.getIdElev());
@@ -66,6 +89,12 @@ public class NotaMapper {
         return nota;
     }
 
+    /**
+     * Actualizează o entitate Nota cu datele dintr-un DTO de tip NotaDTO.
+     *
+     * @param notaDTO DTO-ul care contine noile date pentru entitatea Nota.
+     * @param nota Entitatea Nota care va fi actualizata.
+     */
     public void updateEntityFromDTO(NotaDTO notaDTO, Nota nota) {
 
         Elev elev = elevService.getElevById(notaDTO.getIdElev());
