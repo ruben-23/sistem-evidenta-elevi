0b7a4a13e79f17dcab3bb362401df84d5ad39686
BusecanAntonio <xmaxxantono@yahoo.com>
2025-01-05 17:24:15 +0200

    adaugare javadoc SpecializareServiceImplementare

--- backend/src/main/java/com/liceu/sistem_evidenta_elevi/service/implementare/SpecializareServiceImplementare.java
+++ backend/src/main/java/com/liceu/sistem_evidenta_elevi/service/implementare/SpecializareServiceImplementare.java
@@ -10,27 +10,54 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Implementare a serviciului Specializare.
+ * Contine metode pentru gestionarea operatiunilor legate de specializari.
+ */
 @Service
 public class SpecializareServiceImplementare implements SpecializareService {
 
     private final SpecializareRepository specializareRepository;
 
+    /**
+     * Constructor pentru injectarea dependintei specializareRepository.
+     *
+     * @param specializareRepository Repositorul pentru gestionarea operatiunilor cu specializari.
+     */
     @Autowired
     public SpecializareServiceImplementare(SpecializareRepository specializareRepository) {
         this.specializareRepository = specializareRepository;
     }
 
+    /**
+     * Obtine toate specializarile.
+     *
+     * @return Lista tuturor specializarilor.
+     */
     @Override
     public List<Specializare> getAllSpecializari(){
         return specializareRepository.findAll();
     }
 
+    /**
+     * Obtine o specializare dupa ID.
+     *
+     * @param id ID-ul specializarii.
+     * @return Specializarea corespunzatoare ID-ului.
+     * @throws RuntimeException Daca specializarea nu este gasita.
+     */
     @Override
     public Specializare getSpecializareById(Integer id){
         return specializareRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Specializarea nu a fost gasita"));
     }
 
+    /**
+     * Actualizeaza o specializare pe baza unui SpecializareDTO.
+     *
+     * @param specializareDTO DTO-ul care contine noile date pentru specializare.
+     * @return Specializarea actualizata.
+     */
     @Transactional
     @Override
     public Specializare actualizareSpecializare(SpecializareDTO specializareDTO){
@@ -39,6 +66,12 @@ public class SpecializareServiceImplementare implements SpecializareService {
         return specializareRepository.save(specializareActuala);
     }
 
+    /**
+     * Adauga o specializare in sistem pe baza unui SpecializareDTO.
+     *
+     * @param specializareDTO DTO-ul care contine datele pentru specializare.
+     * @return Specializarea adaugata.
+     */
     @Transactional
     @Override
     public Specializare adaugaSpecializare(SpecializareDTO specializareDTO){
@@ -47,6 +80,11 @@ public class SpecializareServiceImplementare implements SpecializareService {
         return specializareRepository.save(specializare);
     }
 
+    /**
+     * Sterge o specializare din sistem pe baza ID-ului.
+     *
+     * @param idSpecializare ID-ul specializarii de sters.
+     */
     @Override
     public void stergeSpecializare(Integer idSpecializare){
         specializareRepository.deleteById(idSpecializare);
