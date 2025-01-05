3a7d522ae5bfc827ecf73cd08ce838490fa68618
BusecanAntonio <xmaxxantono@yahoo.com>
2025-01-05 17:22:38 +0200

    adaugare javadoc SecretaraRepository

--- backend/src/main/java/com/liceu/sistem_evidenta_elevi/repository/SecretaraRepository.java
+++ backend/src/main/java/com/liceu/sistem_evidenta_elevi/repository/SecretaraRepository.java
@@ -4,6 +4,10 @@ import com.liceu.sistem_evidenta_elevi.entity.Secretara;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository pentru entitatea Secretara.
+ * Aceasta interfata extinde JpaRepository si furnizeaza operatiuni standard de CRUD pentru entitatea Secretara.
+ */
 @Repository
 public interface SecretaraRepository extends JpaRepository<Secretara, Integer> {
 }
