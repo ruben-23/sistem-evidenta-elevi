688d0f6edbf391a4b217cd259e6b1d0eb082d866
BusecanAntonio <xmaxxantono@yahoo.com>
2025-01-05 17:16:50 +0200

    adaugare javadoc BursaMapper

--- backend/src/main/java/com/liceu/sistem_evidenta_elevi/mapper/BursaMapper.java
+++ backend/src/main/java/com/liceu/sistem_evidenta_elevi/mapper/BursaMapper.java
@@ -8,21 +8,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Clasa pentru mapare entitatilor Bursa in DTO
+ * Clasa care transforma entitatile Bursa in DTO-uri si invers.
  */
-
 @Component
 public class BursaMapper {
 
+    /**
+     * Converteste o entitate Bursa intr-un DTO BursaDTO.
+     *
+     * @param bursa Entitatea de convertit.
+     * @return Obiectul BursaDTO rezultat.
+     */
     public BursaDTO toDTO(Bursa bursa) {
         BursaDTO bursaDTO = new BursaDTO();
-
         bursaDTO.setIdBursa(bursa.getIdBursa());
         bursaDTO.setTip(bursa.getTip());
         bursaDTO.setSuma(bursa.getSuma());
         return bursaDTO;
     }
 
+    /**
+     * Converteste o lista de entitati Bursa intr-o lista de DTO-uri BursaDTO.
+     *
+     * @param burse Lista de entitati de convertit.
+     * @return Lista de DTO-uri BursaDTO rezultate.
+     */
     public List<BursaDTO> toDTOList(List<Bursa> burse) {
         List<BursaDTO> bursaDTOs = new ArrayList<>();
         for (Bursa bursa : burse) {
@@ -31,15 +41,26 @@ public class BursaMapper {
         return bursaDTOs;
     }
 
+    /**
+     * Converteste un DTO BursaDTO intr-o entitate Bursa.
+     *
+     * @param bursaDTO DTO-ul de convertit.
+     * @return Entitatea Bursa rezultata.
+     */
     public Bursa toEntity(BursaDTO bursaDTO) {
         Bursa bursa = new Bursa();
         bursa.setIdBursa(bursaDTO.getIdBursa());
         bursa.setTip(bursaDTO.getTip());
         bursa.setSuma(bursaDTO.getSuma());
-
         return bursa;
     }
 
+    /**
+     * Actualizeaza o entitate Bursa cu date dintr-un DTO BursaDTO.
+     *
+     * @param bursaDTO DTO-ul cu noile date.
+     * @param bursa Entitatea de actualizat.
+     */
     public void updateEntityFromDTO(BursaDTO bursaDTO, Bursa bursa) {
         bursa.setIdBursa(bursaDTO.getIdBursa());
         bursa.setTip(bursaDTO.getTip());
